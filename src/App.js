707a146c16b7b69a4cae2474be3ca08ca60e2127
Johan Edenfjord <johan@edenfjord.se>
2020-09-15 09:36:04 +0200

    123

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Header from "./Header";
+import Todos from "./todos";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "hello world",
+        completed: false,
+      },
+      {
+        id: 2,
+        title: "hello worlda",
+        completed: false,
+      },
+      {
+        id: 3,
+        title: "hello worlds",
+        completed: false,
+      },
+    ],
+  };
+  markComplete = (id) => {
+    this.setState({todos: this.state.todos.map(todo =>{
+      if(todo.id === id) {
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    })
+  });
+  };
+
+  delTodo = (id) => {
+    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)]});
+  };
+
+  render() {
+    return <div className="App">
+      <Header />
+      <Todos todos={this.state.todos} markComplete = {this.markComplete} delTodo={this.delTodo}/>
+    </div>;
+  }
 }
 
 export default App;
---
+++ src/addTodo.js
@@ -0,0 +1,14 @@
+import { render } from '@testing-library/react'
+import React, { Component } from 'react'
+
+class addTodo extends Component {
+    render() {
+    return (
+        <div>
+            
+        </div>
+    )
+    }
+}  
+
+export default addTodo;
